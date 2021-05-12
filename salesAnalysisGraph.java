5b63ffedc134d0b0540ca3bc90bd776bd26669db
Tyson <335494555@gapps.yrdsb.ca>
2021-05-12 10:48:59 -0400

    Updating and Commenting
    - Updating the bar graph to be able to call from percentage array
    - Commenting

--- salesAnalysisGraph.java
+++ salesAnalysisGraph.java
@@ -1,4 +1,13 @@
-//Importing JavaFX applications 
+/*
+Due date: 2021, 05, 12
+Date: 2021, 05, 10
+Names: Tyson Z and Vaughn C
+Description: This code will be able receive files, read them, and be able to find the first digit distribution 
+			 according to Benford's Law. After calculating the percentage, the program will display a graph and export a table. 
+			 
+*/
+
+//Importing JavaFX applications for bar graph
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
@@ -12,51 +21,76 @@ import javafx.stage.Stage;
 import java.io.FileReader;
 import java.io.BufferedReader;
 
+//Public application class for JavaFX applications to extend off
 public class salesAnalysisGraph extends Application {
-
+	/*
+	Method Name: main()
+	Description: The main method only launches the JavaFX application
+	@author: Tyson Z
+	*/
     public static void main(String[] args) {
-	Application.launch(args);
+		//Launches the start method
+		Application.launch(args);
     }
-
+	//Override is able to change the javafx packages and input data to display the graph
     @Override
+	/*
+	Method Name: start()
+	Description: Is able to run when the application is launched from main. It takes the role of 'main'
+				 running all the code in the application. The start method will call methods and has the code to display graph
+	@author: Tyson Z
+	@parameter: stage (A function from Javafx package to make stages for display)
+	*/
     public void start(Stage stage) {
+		//Calls the method getSalesData() to read the file and store in int[] sales
+		int[] sales = getSalesData("./sales.csv");
+		//Calls the method getFirstNums() to get the values of each digit and store in int[] firstNums
+		int[] firstNums = getFirstNums(sales);
+		//Calls the method numberToPercentages to calculate the percentages and store in float[] percentages
+		float[] percentages = numberToPercentages(firstNums);
 
-	//Get percentages
-	int[] sales = getSalesData("./sales.csv");
-	int[] firstNums = getFirstNums(sales);
-	float[] percentages = numberToPercentages(firstNums);
+		/*
+		Bar Graph
 
-	//Bar graph
-	stage.setTitle("Graph Stage");
-	//x axis
-	CategoryAxis x = new CategoryAxis();
-	x.setLabel("Benford Digit");
-	//y axis
-	NumberAxis y = new NumberAxis();
-	y.setLabel("Percentage (%)");
-	//bar chart creation
-	BarChart analysisLawGraph = new BarChart(x, y);
-	analysisLawGraph.setTitle("Benford Law Analysis");
-	//add values
-	XYChart.Series benfordGraph = new XYChart.Series();
-	benfordGraph.setName("Percentage of Benford Digit");
-	benfordGraph.getData().add(new XYChart.Data("1", 43));
-	benfordGraph.getData().add(new XYChart.Data("2", 25));
-	benfordGraph.getData().add(new XYChart.Data("3", 10));
-	benfordGraph.getData().add(new XYChart.Data("4", 33));
-	benfordGraph.getData().add(new XYChart.Data("5", 25));
-	benfordGraph.getData().add(new XYChart.Data("6", 9));
-	benfordGraph.getData().add(new XYChart.Data("7", 33));
-	benfordGraph.getData().add(new XYChart.Data("8", 25));
-	benfordGraph.getData().add(new XYChart.Data("9", 10));
-	analysisLawGraph.getData().add(benfordGraph);
-	//vertical box
-	VBox vbox = new VBox(analysisLawGraph);
-	Scene sc = new Scene(vbox, 800, 700);
-	stage.setScene(sc);
-	stage.setHeight(500);
-	stage.setWidth(600);
-	stage.show();
+		This portion of the start method will be the part where the bar graph is made and then displayed. The values will be 
+		passed from the methods called above to be used as the values for the bar graph
+		*/
+		//Creates a new x/Category Axis of the bar graph
+		CategoryAxis x = new CategoryAxis();
+		//Names the label of x Axis
+		x.setLabel("First Digit");
+		//Creates a new y/Number Axis of the bar graph
+		NumberAxis y = new NumberAxis();
+		//Names the label of y Axis
+		y.setLabel("Percentage (%)");
+		//Creates a new bar graph with axes
+		BarChart analysisLawGraph = new BarChart(x, y);
+		//Sets the title of the bar graph to be displayed
+		analysisLawGraph.setTitle("Sales Distribution Graphical Dislpay");
+		//Creating a new series to store the data
+		XYChart.Series benfordGraph = new XYChart.Series();
+		//Names the series for the legend
+		benfordGraph.setName("Percentage of the First Digit");
+		//For loop that is able to call values from percentages and place them into series
+		for (int k = 0; k < percentages.length; k++){
+			//Converts the for loop value from int to string for Benford first digit
+			String benfordDigit = String.valueOf(k+1);
+			//Calls the percentages for digits and adds them to series 
+			benfordGraph.getData().add(new XYChart.Data(benfordDigit,percentages[k]));
+		}
+		//Adds the data of series to the bar graph 
+		analysisLawGraph.getData().add(benfordGraph);
+		//Creates a new Vertical box to place the bar graph
+		VBox vbox = new VBox(analysisLawGraph);
+		//Creates a new scene with the bar graph in it
+		Scene sc = new Scene(vbox, 800, 900);
+		//Adds the new scene to the one from JavaFx packages
+		stage.setScene(sc);
+		//Sets the original scene to dimensions
+		stage.setHeight(400);
+		stage.setWidth(600);
+		//Displays the stage with the bar graph in it
+		stage.show();
     }
 
     /**
@@ -71,56 +105,56 @@ public class salesAnalysisGraph extends Application {
      */
     public static int[] getSalesData(String csvFileName) {
 
-	String data = "";					// csvFileName will be parsed to this string
-	
-	/*
-	 * This section parses the file into a string by reading each line in the
-	 * file and putting it into the string. Since each row doesn't have a
-	 * comma seperating a row's value and the next row after that, it has to 
-	 * manually add it to the string.	
-	 */
-	try {
-	    BufferedReader fileReader = new BufferedReader(new FileReader(csvFileName));
-	    
-	    String fileLine;					// Represents the current line being parsed	    
-	    int fileLineNum = 1;				// Represents the current line number
+		String data = "";					// csvFileName will be parsed to this string
+		
+		/*
+		* This section parses the file into a string by reading each line in the
+		* file and putting it into the string. Since each row doesn't have a
+		* comma seperating a row's value and the next row after that, it has to 
+		* manually add it to the string.	
+		*/
+		try {
+			BufferedReader fileReader = new BufferedReader(new FileReader(csvFileName));
+			
+			String fileLine;					// Represents the current line being parsed	    
+			int fileLineNum = 1;				// Represents the current line number
 
-	    while ((fileLine = fileReader.readLine()) != null) {
+			while ((fileLine = fileReader.readLine()) != null) {
 
-		if (fileLineNum != 1) {				// Don't include the header at row 
-		    data = data + fileLine + ",";		// Add comma delimter
+			if (fileLineNum != 1) {				// Don't include the header at row 
+				data = data + fileLine + ",";		// Add comma delimter
+			}
+			
+			fileLineNum++;					// Represents the next line number
+			}
+
+			data = data.substring(0, data.length() - 1);	// Remove the last comma from data string
+
+			fileReader.close();
+			
+		} catch (Exception e) {					// Report error in case anything goes wrong,
+			System.out.println("Something went wrong:");	// such as the file being unavailable.
+			System.out.println(e.getMessage());
+		}
+
+		/*
+		* This section splits the values seperated by commas within the data 
+		* string into an array of String values. An integer array will be 
+		* made at half the size of the string array to only get the sales
+		* data.
+		*/
+		String[] salesArray = data.split(",");			// Split the data string into an array
+
+		int[] sales = new int[salesArray.length / 2];		// Array to hold all the sales data
+
+		int salesElement = 0;					// Holds which index to hold the sales data in
+		
+		for (int i = 1; i < salesArray.length; i +=2) {			// i starts at 1 because all sales data
+			sales[salesElement] = Integer.parseInt(salesArray[i]);	// is stored in the second column
+			salesElement++;
 		}
 		
-		fileLineNum++;					// Represents the next line number
-	    }
-
-	    data = data.substring(0, data.length() - 1);	// Remove the last comma from data string
-
-	    fileReader.close();
-	    
-	} catch (Exception e) {					// Report error in case anything goes wrong,
-	    System.out.println("Something went wrong:");	// such as the file being unavailable.
-	    System.out.println(e.getMessage());
-	}
-
-	/*
-	 * This section splits the values seperated by commas within the data 
-	 * string into an array of String values. An integer array will be 
-	 * made at half the size of the string array to only get the sales
-	 * data.
-	 */
-	String[] salesArray = data.split(",");			// Split the data string into an array
-
-	int[] sales = new int[salesArray.length / 2];		// Array to hold all the sales data
-
-	int salesElement = 0;					// Holds which index to hold the sales data in
-	
-	for (int i = 1; i < salesArray.length; i +=2) {			// i starts at 1 because all sales data
-	    sales[salesElement] = Integer.parseInt(salesArray[i]);	// is stored in the second column
-	    salesElement++;
-	}
-	
-	return sales;
+		return sales;
     }
 
     /**
@@ -137,37 +171,37 @@ public class salesAnalysisGraph extends Application {
      */
     public static int[] getFirstNums(int[] numbers) {
 
-	/*
-	 * The strategy for getting the first numbers is to iterate through all
-	 * the numbers 9 times from 1 to 9. For each number, it checks the first
-	 * digit and, if it is equal to the number being checked, it is added to 
-	 * the array of the same index.	
-	 */
-	int[] firstNums = new int[9];				// Array consisting frequency of numbers
-								// from 1 to 9.
-
-	for (int i = 0; i < 9; i++) {
-	    for (int number : numbers) {
-		
 		/*
-		 * Integer -> String -> Character -> Integer
-		 * 	Each number gets converted to a string. Using the function
-		 * 	'charAt(0)', I can get the first element of the string, but
-		 * 	as a character. Then I use a character method to convert
-		 * 	a characater value back into an integer.
-		 * 
-		 * For example, say that 45638 was the number to check:
-		 * 	(int) 45638 -> (string) "45638" -> (char) '4' -> (int) 4
-		 */
-		int firstDigit = Character.getNumericValue(Integer.toString(number).charAt(0));
+		* The strategy for getting the first numbers is to iterate through all
+		* the numbers 9 times from 1 to 9. For each number, it checks the first
+		* digit and, if it is equal to the number being checked, it is added to 
+		* the array of the same index.	
+		*/
+		int[] firstNums = new int[9];				// Array consisting frequency of numbers
+									// from 1 to 9.
 
-		if (firstDigit == i + 1) {			// Add 1, otherwise it would check
-		    firstNums[i]++;				// numbers 0-8 instead.
+		for (int i = 0; i < 9; i++) {
+			for (int number : numbers) {
+			
+			/*
+			* Integer -> String -> Character -> Integer
+			* 	Each number gets converted to a string. Using the function
+			* 	'charAt(0)', I can get the first element of the string, but
+			* 	as a character. Then I use a character method to convert
+			* 	a characater value back into an integer.
+			* 
+			* For example, say that 45638 was the number to check:
+			* 	(int) 45638 -> (string) "45638" -> (char) '4' -> (int) 4
+			*/
+				int firstDigit = Character.getNumericValue(Integer.toString(number).charAt(0));
+
+				if (firstDigit == i + 1) {			// Add 1, otherwise it would check
+					firstNums[i]++;				// numbers 0-8 instead.
+				}
+			}
 		}
-	    }
-	}
-	
-	return firstNums;
+		
+		return firstNums;
     }
 
         /**
@@ -177,20 +211,20 @@ public class salesAnalysisGraph extends Application {
      */
     public static float[] numberToPercentages(int[] numbers) {
 
-	int numTotal = 0;
-	
-	for (int number : numbers) {				// Add sum of all numbers in total
-	    numTotal += number;
-	}
+		int numTotal = 0;
+		
+		for (int number : numbers) {				// Add sum of all numbers in total
+			numTotal += number;
+		}
 
-	float[] percentages = new float[numbers.length];	// Size of array of percentage is the
-								// same as size of array of numbers
-	
-	for (int i = 0; i < numbers.length; i++) {
-	    percentages[i] = (float) numbers[i] / numTotal * 100;	// Divide number to total for decimal
-	    								// and multiply by 100 for percentage
-	}
+		float[] percentages = new float[numbers.length];	// Size of array of percentage is the
+									// same as size of array of numbers
+		
+		for (int i = 0; i < numbers.length; i++) {
+			percentages[i] = (float) numbers[i] / numTotal * 100;	// Divide number to total for decimal
+											// and multiply by 100 for percentage
+		}
 
-	return percentages;
+		return percentages;
     }
 }
