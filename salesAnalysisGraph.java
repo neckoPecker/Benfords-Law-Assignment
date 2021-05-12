85980d9a3b8e04c3e7eb99acfc017582d6f60555
Ducky <masterless123@gmail.com>
2021-05-12 11:15:55 -0400

    Remove branch merge artifact
    I accidently left a merge artifact in the code. This should make the
    code runnable again.

--- salesAnalysisGraph.java
+++ salesAnalysisGraph.java
@@ -105,41 +105,6 @@ public class salesAnalysisGraph extends Application {
 		stage.setWidth(600);
 		//Displays the stage with the bar graph in it
 		stage.show();
-=======
-
-
-	//Bar graph
-	stage.setTitle("Graph Stage");
-	//x axis
-	CategoryAxis x = new CategoryAxis();
-	x.setLabel("Benford Digit");
-	//y axis
-	NumberAxis y = new NumberAxis();
-	y.setLabel("Percentage (%)");
-	//bar chart creation
-	BarChart analysisLawGraph = new BarChart(x, y);
-	analysisLawGraph.setTitle("Benford Law Analysis");
-	//add values
-	XYChart.Series benfordGraph = new XYChart.Series();
-	benfordGraph.setName("Percentage of Benford Digit");
-	benfordGraph.getData().add(new XYChart.Data("1", 43));
-	benfordGraph.getData().add(new XYChart.Data("2", 25));
-	benfordGraph.getData().add(new XYChart.Data("3", 10));
-	benfordGraph.getData().add(new XYChart.Data("4", 33));
-	benfordGraph.getData().add(new XYChart.Data("5", 25));
-	benfordGraph.getData().add(new XYChart.Data("6", 9));
-	benfordGraph.getData().add(new XYChart.Data("7", 33));
-	benfordGraph.getData().add(new XYChart.Data("8", 25));
-	benfordGraph.getData().add(new XYChart.Data("9", 10));
-	analysisLawGraph.getData().add(benfordGraph);
-	//vertical box
-	VBox vbox = new VBox(analysisLawGraph);
-	Scene sc = new Scene(vbox, 800, 700);
-	stage.setScene(sc);
-	stage.setHeight(500);
-	stage.setWidth(600);
-	stage.show();
->>>>>>> file-operations
     }
 
     /**
